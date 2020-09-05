195b8f9dfecedeac0aacffefdcaba429ac64bb10
Drew Pham <drewvpham@gmail.com>
2020-09-05 16:14:52 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDh8ezi-QzpWwbLOsvgdFgiJT0kEY2BZtM",
+  authDomain: "clone-1b598.firebaseapp.com",
+  databaseURL: "https://clone-1b598.firebaseio.com",
+  projectId: "clone-1b598",
+  storageBucket: "clone-1b598.appspot.com",
+  messagingSenderId: "928630468729",
+  appId: "1:928630468729:web:fe49ca343de6fbef9a24a8",
+  measurementId: "G-RDZC75PVHS"
+};
